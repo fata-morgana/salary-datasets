1ab55500b699a333604f517abafc87b58a3772ef
Kopkaj Oupapatig <kpkj.ou@gmail.com>
2023-07-09 14:18:06 +0700

    Add Gender 'Blank' if the field value is empty

--- src/main/java/org/kopkaj/salarydataset/model/Gender.java
+++ src/main/java/org/kopkaj/salarydataset/model/Gender.java
@@ -12,14 +12,14 @@ public enum Gender {
         this.description = description;
     }
 
-    public static Gender parseFromString(String input) {
-        if (BLANK.description.equalsIgnoreCase(input)) {
+    public static Gender parseFromString(final String input) {
+        if (BLANK.description.equalsIgnoreCase(input.trim())) {
             return BLANK;
         }
-        else if (MALE.description.equalsIgnoreCase(input)) {
+        else if (MALE.description.equalsIgnoreCase(input.trim())) {
             return MALE;
         }
-        else if (FEMALE.description.equalsIgnoreCase(input)) {
+        else if (FEMALE.description.equalsIgnoreCase(input.trim())) {
             return FEMALE;
         }
         else return NON_BINARY; // fallback case. we need to support LGBTQ people
