811426f06019fe5a0f0c6bbaf5a6285357a2ec0b
Kopkaj Oupapatig <kpkj.ou@gmail.com>
2023-07-09 16:11:59 +0700

    Controller now return customized json string from result object

--- src/main/java/org/kopkaj/salarydataset/controller/SalaryController.java
+++ src/main/java/org/kopkaj/salarydataset/controller/SalaryController.java
@@ -1,9 +1,13 @@
 package org.kopkaj.salarydataset.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.kopkaj.salarydataset.model.SalaryDataset;
 import org.kopkaj.salarydataset.service.SalaryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedHashMap;
@@ -20,13 +24,20 @@ public class SalaryController {
     }
 
     private static final Logger logger = LoggerFactory.getLogger(SalaryController.class);
-    @GetMapping("job_data")
-    public @ResponseBody List<SalaryDataset> get(@RequestParam LinkedHashMap<String,String> allRequestParams) {
+    @GetMapping(path="job_data", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody String get(@RequestParam LinkedHashMap<String,String> allRequestParams) {
         logger.info("request: " + allRequestParams);
         List<SalaryDataset> result = salaryService.queryWithParameters(allRequestParams);
         logger.info("start of response...");
         result.stream().forEach(i -> logger.info(i.toString()));
         logger.info("end of response...");
-        return result;
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        try {
+            return objectMapper.writeValueAsString(result);
+        } catch (JsonProcessingException e) {
+            logger.error("Cannot serialize result to json object.", e);
+            throw new RuntimeException(e);
+        }
     }
 }
